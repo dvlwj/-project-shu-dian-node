e33b9b1fc011ef2c03160b15fd121871e0a4f122
dvlwj <dvlimweijiang@gmail.com>
2020-05-20 00:52:42 +0700

    update routes to fix details routing

--- routes.js
+++ routes.js
@@ -3,11 +3,10 @@
 module.exports = function(app) {
     var routeGeneral = require('./app/controllers/generals/controller');
     var routeList = require('./app/controllers/list/controller');
-    var routeDetails = require('./app/controllers/details/controller');
     app.route('/')
         .get(routeGeneral.index);
     app.route('/list')
         .get(routeList.get);
-    app.route('/list/{id}')
-        .get(routeDetails.get);
+    app.route('/list/getDetails')
+        .post(routeList.getDetails);
 };

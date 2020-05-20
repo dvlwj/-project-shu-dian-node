88b0a388863672f1b5f9c2c1007c05dc66a1a0d0
dvlwj <dvlimweijiang@gmail.com>
2020-05-21 00:53:28 +0700

    fix routes.js linting

--- routes.js
+++ routes.js
@@ -1,14 +1,12 @@
-'use strict';
-
-module.exports = function(app) {
-    var routeGeneral = require('./app/controllers/generals/controller');
-    var routeList = require('./app/controllers/list/controller');
-    app.route('/')
-        .get(routeGeneral.index);
-    app.route('/list')
-        .get(routeList.get);
-    app.route('/list/getDetails')
-        .post(routeList.getDetails);
-    app.route('/list/delete')
-        .delete(routeList.delete);
+module.exports = function route(app) {
+  const routeGeneral = require('./app/controllers/generals/controller');
+  const routeList = require('./app/controllers/list/controller');
+  app.route('/')
+    .get(routeGeneral.index);
+  app.route('/list')
+    .get(routeList.get);
+  app.route('/list/getDetails')
+    .post(routeList.getDetails);
+  app.route('/list/delete')
+    .delete(routeList.delete);
 };

27730e47a7e9de165b50b579e59783ae88d60bea
dvlwj <dvlimweijiang@gmail.com>
2020-05-21 02:01:27 +0700

    add new common response structure

--- app/utils/commonResponseStructure.js
+++ app/utils/commonResponseStructure.js
@@ -1,30 +1,36 @@
-exports.ok = function ok(values, res) {
-  let data = null;
-  if (values.length > 1) {
-    data = {
-      status: 200,
-      data: values,
-    };
-  } else {
-    data = {
-      status: 200,
-      data: values[0],
-    };
-  }
+exports.ok = function ok(values, message, res) {
+  const data = {
+    message,
+    data: values.length > 1 ? values : values[0],
+  };
+  res.status(200);
   res.json(data);
   res.end();
 };
 
-exports.error = function error(message) {
+exports.error = function error(message, res) {
   const data = {
-    status: 500,
     message,
   };
+  res.status(500);
+  res.json(data);
+  res.end();
 };
 
-exports.timeout = function timeout(message) {
-  let data = {
-    status: 408,
+exports.timeout = function timeout(message, res) {
+  const data = {
     message,
   };
+  res.status(408);
+  res.json(data);
+  res.end();
+};
+
+exports.empty = function empty(message, res) {
+  const data = {
+    message,
+  };
+  res.status(204);
+  res.json(data);
+  res.end();
 };

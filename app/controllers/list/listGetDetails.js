e0fee8922768db777e037732b530c1586ce2a3df
dvlwj <dvlimweijiang@gmail.com>
2020-05-21 02:26:12 +0700

    improving feature of get details list endpoint to the new standard

--- app/controllers/list/listGetDetails.js
+++ app/controllers/list/listGetDetails.js
@@ -13,28 +13,33 @@ exports.getDetails = function getDetails(req, res) {
       },
       [requestPayloadBody.id],
       (error, rows) => {
-        if (error) {
-          console.log(error);
-        } else if (error && error.code === 'PROTOCOL_SEQUENCE_TIMEOUT') {
-          console.log('===== Failed to query data details from table to_do_list : timeout');
-          res.timeout('===== Failed to get data. Data processing take so long !');
-        } else if (rows.length === 0) {
+        if (error && error.code === 'PROTOCOL_SEQUENCE_TIMEOUT') {
+          const message = 'Failed to get data. Data processing take so long !';
+          console.log('===== Failed to query data from table to_do_list : timeout');
+          response.timeout(message, res);
+        } else if (error) {
+          const message = 'Failed to process your request, some internal error';
+          console.log('===== Failed to process request, some internal error happening : ', error);
+          response.error(message, res);
+        } else if (!error && rows.length === 0) {
           console.log('===== Success queried data from table to_do_list, but no data found');
-          response.empty('No data found', res);
+          response.empty(res);
         } else {
           const data = rows;
           const dataInJSONString = JSON.stringify(data);
           const dataInJSONStringMinimified = dataInJSONString.replace(/\n/g, '');
+          const message = 'Successfully get data';
           console.log('===== Success queried data details from table to_do_list');
           console.log('===== Queried data from db to return :', dataInJSONStringMinimified);
-          response.ok(rows, res);
+          response.ok(rows, message, res);
         }
         console.log('==== Stop process of /list/getDetails endpoint');
       },
     );
   } catch (error) {
-    console.log(`===== Process of /list/getDetails endpoint got an issue :${error}`);
-    response.error(error);
+    const message = 'Failed to process your request, some internal error';
+    console.log('===== Process of /list/getDetails endpoint got an issue :', error);
+    response.error(message, error);
     console.log('==== Stop process of /list/getDetails endpoint');
   }
 };

7f81a3f7f243ef7e5ad09fd3bf8c6aef6634d7af
dvlwj <dvlimweijiang@gmail.com>
2020-05-21 02:24:42 +0700

    add response : content empty (204), forbidden(403), n unauthorized (401)

--- app/utils/commonResponseStructure.js
+++ app/utils/commonResponseStructure.js
@@ -26,11 +26,25 @@ exports.timeout = function timeout(message, res) {
   res.end();
 };
 
-exports.empty = function empty(message, res) {
+exports.empty = function empty(res) {
+  res.status(204);
+  res.end();
+};
+
+exports.forbidden = function forbidden(message, res) {
   const data = {
     message,
   };
-  res.status(204);
+  res.status(403);
+  res.json(data);
+  res.end();
+};
+
+exports.unauthorized = function unauthorized(res) {
+  const data = {
+    message: 'Credentials invalid. Please try again with valid credentials',
+  };
+  res.status(401);
   res.json(data);
   res.end();
 };

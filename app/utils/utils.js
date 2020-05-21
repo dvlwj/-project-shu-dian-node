58b85958c809f7fd613b652aa5b03873e5591a49
dvlwj <dvlimweijiang@gmail.com>
2020-05-21 21:32:09 +0700

    add a common function to check authentication validity

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,6 +1,17 @@
+const response = require('./commonResponseStructure');
+const logger = require('./logger');
+
 exports.checkIfObjectEmpty = function checkIfObjectEmpty(data) {
   return Object.keys(data).length === 0;
 };
 exports.checkIfPropertyEmpty = function checkIfPropertyEmpty(data) {
   return data === undefined;
+};
+exports.checkIfAuthenticated = function checkIfAuthenticated(req, res) {
+  if (req.session.uniqueID === req.body.sessionID && req.session.authenticated) {
+    logger.info('Credentials verified, will let\' request continue');
+    return true;
+  }
+  logger.info('Credentials is wrong, stop request');
+  response.unauthorized(res);
 };

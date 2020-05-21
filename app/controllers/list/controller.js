cf3885ec11c3909fdd65796b89cfbe16ea14d2d5
dvlwj <dvlimweijiang@gmail.com>
2020-05-21 11:17:30 +0700

    add listUpdate in controller

--- app/controllers/list/controller.js
+++ app/controllers/list/controller.js
@@ -1,7 +1,9 @@
 const listGet = require('./listGet');
 const listGetDetails = require('./listGetDetails');
 const listDelete = require('./listDelete');
+const listUpdate = require('./listUpdate');
 
 exports.get = listGet.get;
 exports.getDetails = listGetDetails.getDetails;
 exports.delete = listDelete.delete;
+exports.update = listUpdate.updateDetails;

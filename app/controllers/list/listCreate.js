46480cb83ccc61e0317f068bccd0295f799f4822
dvlwj <dvlimweijiang@gmail.com>
2020-05-21 15:35:57 +0700

    refactor logging to follow new log format

--- app/controllers/list/listCreate.js
+++ app/controllers/list/listCreate.js
@@ -1,24 +1,25 @@
 const response = require('../../utils/commonResponseStructure');
 const connection = require('../../utils/connection');
 const utils = require('../../utils/utils');
+const logger = require('../../utils/logger');
 
 exports.create = function createData(req, res) {
   const requestPayloadBody = req.body;
   const requestPayloadBodyInString = JSON.stringify(requestPayloadBody);
-  console.log(`==== Start process of /list/create endpoint with request payload : ${requestPayloadBodyInString}`);
+  logger.info(`Start process of /list/create endpoint with request payload : ${requestPayloadBodyInString}`);
   if (utils.checkIfObjectEmpty(requestPayloadBody)) {
     const message = 'Request payload can\'t be empty';
-    console.log(`Request is rejected because ${message}`);
+    logger.info(`Request is rejected because ${message}`);
     response.badRequest(message, res);
   } else if (
     utils.checkIfPropertyEmpty(requestPayloadBody.subject)
     || typeof requestPayloadBody.subject !== 'string'
   ) {
     const message = 'Subject shouldn\'t be empty and must be in string';
-    console.log(`Request is rejected because ${message}`);
+    logger.info(`Request is rejected because ${message}`);
     response.badRequest(message, res);
   } else {
-    console.log('===== Starting to run the query to create new data in to_do_list table');
+    logger.info('Starting to run the query to create new data in to_do_list table');
     try {
       connection.query(
         {
@@ -29,36 +30,36 @@ exports.create = function createData(req, res) {
         (error, rows) => {
           if (error && error.code === 'PROTOCOL_SEQUENCE_TIMEOUT') {
             const message = 'Failed to get data. Data processing take so long !';
-            console.log('===== Failed to query data from table to_do_list : timeout');
+            logger.info('Failed to query data from table to_do_list : timeout');
             response.timeout(message, res);
           } else if (error) {
             const message = 'Failed to process your request, some internal error';
-            console.log('===== Failed to process request, some internal error happening : ', error);
+            logger.info(`Failed to process request, some internal error happening : ${error}`);
             response.error(message, res);
           } else if (!error && rows.length === 0) {
-            console.log('===== Success queried data from table to_do_list, but no data found');
+            logger.info('Success queried data from table to_do_list, but no data found');
             response.empty(res);
           } else if (!error && rows.affectedRows === 0) {
             const message = 'Failed to process your request, no data affected';
-            console.log('===== Failed to process request, no data affected');
+            logger.info('Failed to process request, no data affected');
             response.forbidden(message, res);
           } else {
             const data = rows;
             const dataInJSONString = JSON.stringify(data);
             const dataInJSONStringMinimified = dataInJSONString.replace(/\n/g, '');
             const message = 'Data has been successfully created';
-            console.log('===== Success queried data details from table to_do_list');
-            console.log('===== Queried data from db to return :', dataInJSONStringMinimified);
+            logger.info('Success queried data details from table to_do_list');
+            logger.info(`Queried data from db to return : ${dataInJSONStringMinimified}`);
             response.ok(rows, message, res);
           }
-          console.log('==== Stop process of /list/create endpoint');
+          logger.info('Stop process of /list/create endpoint');
         },
       );
     } catch (error) {
       const message = 'Failed to process your request, some internal error';
-      console.log('===== Process of /list/create endpoint got an issue :', error);
+      logger.info(`Process of /list/create endpoint got an issue : ${error}`);
       response.error(message, error);
     }
   }
-  console.log('==== Stop process of /list/create endpoint');
+  logger.info('Stop process of /list/create endpoint');
 };
--- app/controllers/list/listDelete.js
+++ app/controllers/list/listDelete.js
@@ -1,9 +1,10 @@
 const response = require('../../utils/commonResponseStructure');
 const connection = require('../../utils/connection');
+const logger = require('../../utils/logger');
 
 exports.delete = function deleteData(req, res) {
-  console.log('==== Start process of /list/delete endpoint');
-  console.log('===== Starting to run the query to delete data in to_do_list table');
+  logger.info('Start process of /list/delete endpoint');
+  logger.info('Starting to run the query to delete data in to_do_list table');
   try {
     const requestPayloadBody = req.body;
     connection.query(
@@ -15,32 +16,32 @@ exports.delete = function deleteData(req, res) {
       (error, rows) => {
         if (error && error.code === 'PROTOCOL_SEQUENCE_TIMEOUT') {
           const message = 'Failed to get data. Data processing take so long !';
-          console.log('===== Failed to query data from table to_do_list : timeout');
+          logger.info('Failed to query data from table to_do_list : timeout');
           response.timeout(message, res);
         } else if (error) {
           const message = 'Failed to process your request, some internal error';
-          console.log('===== Failed to process request, some internal error happening : ', error);
+          logger.info(`Failed to process request, some internal error happening : ${error}`);
           response.error(message, res);
         } else if (!error && rows.affectedRows === 0) {
           const message = 'Failed to process your request, no data affected';
-          console.log('===== Failed to process request, no data affected');
+          logger.info('Failed to process request, no data affected');
           response.forbidden(message, res);
         } else {
           const data = rows;
           const dataInJSONString = JSON.stringify(data);
           const dataInJSONStringMinimified = dataInJSONString.replace(/\n/g, '');
           const message = 'Success delete data from table to_do_lis';
-          console.log('===== Success delete data from table to_do_list');
-          console.log('===== Queried data from db to return :', dataInJSONStringMinimified);
+          logger.info('Success delete data from table to_do_list');
+          logger.info(`Queried data from db to return : ${dataInJSONStringMinimified}`);
           response.ok(rows, message, res);
         }
-        console.log('==== Stop process of /list/delete endpoint');
+        logger.info('Stop process of /list/delete endpoint');
       },
     );
   } catch (error) {
     const message = 'Failed to process your request, some internal error';
-    console.log('===== Process of /list/delete endpoint got an issue : ', error);
+    logger.info(`Process of /list/delete endpoint got an issue : ${error}`);
     response.error(message, error);
-    console.log('==== Stop process of /list/delete endpoint');
+    logger.info('Stop process of /list/delete endpoint');
   }
 };

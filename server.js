77d986cde6ecde182f78c12f949b9b69ecf3b3cb
dvlwj <dvlimweijiang@gmail.com>
2020-05-25 01:29:37 +0700

    set express js to return cache control with max age

--- server.js
+++ server.js
@@ -13,6 +13,22 @@ const uniqueID = uuid.v4();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static('public', {
+  etag: true, // Just being explicit about the default.
+  lastModified: true, // Just being explicit about the default.
+  setHeaders: (res, path) => {
+    const hashRegExp = new RegExp('\\.[0-9a-f]{8}\\.');
+
+    if (path.endsWith('.html')) {
+      // All of the project's HTML files end in .html
+      res.setHeader('Cache-Control', 'no-cache');
+    } else if (hashRegExp.test(path)) {
+      // If the RegExp matched, then we have a versioned URL.
+      res.setHeader('Cache-Control', 'max-age=31536000');
+    }
+  },
+}));
+
 // app.options('*', cors());
 const allowedOrigins = [
   'http://localhost:3000',
